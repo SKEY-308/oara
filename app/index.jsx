d5ead90fd0b4d99e5e6abcbea34fc6d0f8a61967
Your Name <severus.key308@gmail.com>
2024-04-25 20:02:22 +0100

    20'02

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl">Oara..!!</Text>
+            <Text className="text-3xl font-pblack">Oara..!!</Text>
             <StatusBar style="auto" />
             <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
         </View>
